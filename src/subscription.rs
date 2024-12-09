7bc05018bb3fc8ad85ea9fcef292b501f4a0288f
Gael <69738055+gaelo-dev@users.noreply.github.com>
2024-12-09 12:19:18 -0600

    Changes in subscription bluetooth, ConnectionState is now part of this module

--- src/subscription.rs
+++ src/subscription.rs
@@ -1,50 +1,31 @@
-use crate::bluetooth::{self, ConnectionState, Command};
-use crate::Config;
+use crate::bluetooth::{self, Command, BDAddr};
 
 use iced::{
     futures::{
-        channel::mpsc, sink::SinkExt, Stream, StreamExt
+        channel::mpsc, sink::SinkExt, Stream, StreamExt,
     },
     stream, 
 };
 
-#[derive(Debug, Clone)]
-pub enum Event {
-    Connected,
-    CommandReceived(Command),
-    Disconnected{
-        peripherals: bluetooth::Peripherals
-    },
-    Err(String),
-    Ready(mpsc::Sender<Input>),
-    // ...
-}
-
-#[derive(Debug, Clone)]
-pub enum Input {
-    Connect(bluetooth::BDAddr),
-    Command(bluetooth::Command),
-    // ...
-}
-
 pub fn connection() -> impl Stream<Item = Event> {
     stream::channel(100, |mut output| async move {
-        let mut state = ConnectionState::Loading;
-
-        let (sender, mut receiver) = mpsc::channel(100);        
-        let _= output.send(Event::Ready(sender)).await;
+        let (sender, receiver) = mpsc::channel(100);
         
+        let central = bluetooth::Central::new().await.unwrap();
+        let peripherals = central.scan().await.unwrap();
+
+        let _ = output.send(Event::Disconnected { peripherals: peripherals.clone(), sender }).await;
+        let mut state = ConnectionState::Disconnected { peripherals, receiver };
+
         loop {
             match &mut state {
-                ConnectionState::Connected { xplorer, .. } => {
+                ConnectionState::Connected { xplorer, receiver } => {
                     let mut notifications = xplorer.notifications().await.unwrap();
 
                     iced::futures::select! {
-                        input = receiver.select_next_some() => {
-                            if let Input::Command(cmd) = input {
-                                if let Err(err) = xplorer.send(cmd).await {
-                                    let _= output.send(Event::Err(err.to_string())).await;
-                                }
+                        cmd = receiver.select_next_some() => {
+                            if let Err(err) = xplorer.send(cmd).await {
+                                let _= output.send(Event::Err(err.to_string())).await;
                             }
                         }
 
@@ -55,43 +36,55 @@ pub fn connection() -> impl Stream<Item = Event> {
                         
                     }
                 },
-                ConnectionState::Disconnected { .. } => {
-                    if let Input::Connect(addr) = receiver.select_next_some().await {       
-                        println!("trying connect to: {addr}");                 
-                        state = match state.clone().reconnect(addr).await {
-                            Ok(s) => {
-                                let _= output.send(Event::Connected).await;
-                                s
-                            },
-                            Err(err) => {
-                                let _= output.send(Event::Err(err.to_string())).await;
-                                state
-                            }
-                        }
-                    }
-                },
-                ConnectionState::Loading => {
-                    let cfg = Config::load().unwrap();
+                ConnectionState::Disconnected { peripherals, receiver, .. } => {
+                    let addr = receiver.select_next_some().await;               
 
-                    match bluetooth::start(cfg.addr).await {
-                        Ok(s) => {
-                            state = s;
-                            match &state {
-                                ConnectionState::Connected { .. } => {
-                                    let _ = output.send(Event::Connected).await;
-                                },
-                                ConnectionState::Disconnected { peripherals, .. } => {
-                                    let _ = output.send(Event::Disconnected { peripherals: peripherals.clone() }).await;
-                                }
-                                _ => ()
-                            }
-                        },
-                        Err(err) => {
-                            let _= output.send(Event::Err(err.to_string())).await;
-                        }
-                    }
+                    let (id, _properties) = peripherals
+                        .into_iter()
+                        .find(|(_id, properties)| properties.address == addr)
+                        .ok_or(bluetooth::BlueError::NotFoundPeripheral)
+                        .unwrap();
+                    
+                    let xplorer = central.connect(&id).await.unwrap();
+                    let (sender, receiver) = mpsc::channel(100);
+                    
+                    let _ = output.send(Event::Connected { addr, sender }).await;
+                    state = ConnectionState::Connected { xplorer, receiver };
                 },
             }
         }
     })
+}
+
+#[derive(Debug, Clone)]
+pub enum Event {
+    Connected {
+        addr: BDAddr,
+        sender: mpsc::Sender<Command>,
+    },
+    CommandReceived(Command),
+    Disconnected {
+        peripherals: bluetooth::Peripherals,
+        sender: mpsc::Sender<bluetooth::BDAddr>,
+    },
+    Err(String),
+    // ...
+}
+
+/// Represents the connection state of the Bluetooth communication
+#[derive(Debug)]
+enum ConnectionState {
+    /// Represents the connected state and contains the peripheral to which it is connected.
+    Connected {
+        // central: Central,
+        xplorer: bluetooth::Xplorer,
+        receiver: mpsc::Receiver<Command>,
+    },
+
+    /// Represents the disconnected state and contains a list of peripherals to which it can connect.
+    Disconnected {
+        // central: Central,
+        peripherals: bluetooth::Peripherals,
+        receiver: mpsc::Receiver<BDAddr>
+    },
 }
